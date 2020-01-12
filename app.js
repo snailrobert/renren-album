c086d2e34d81d30aeb1c7e775b67587708fa52f5
chenjun <chenjun@ydcfo.com>
2020-01-12 17:17:51 +0800

    1、读取本地json文件批量下载相册 2、先注释掉下载原图机会，优先下载大图

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const program = require('commander')
 const login = require('./login')
 const photo = require('./photo')
 const keyword = require('./keyword')
+const config = require('./config')
+
+const fs =require('fs')
 
 program
   .version(package.version)
@@ -18,6 +21,9 @@ program
   .option('-k, --keyword [string]', 'append keyword')
   .option('-n, --number [number]', 'the number of keyword images you want to download, default 10')
   .option('-i, --userid [string]', 'specify download someone\'s album by user id')
+  .option('-a, --friend', 'find my friends')
+  .option('-b, --follow', 'find follow me')
+  .option('-m, --profile', 'log my profile')
   .parse(process.argv)
 
 async function run() {
@@ -32,12 +38,45 @@ async function run() {
   } else if (program.download) {
     if (program.userid === undefined) {
       await photo.getSelfAlbum()
+    } else if (program.userid === '123') {
+      const renrenallfriendsPath = path.resolve(config.storageDir, 'renrenallfriends.json')
+      console.log(renrenallfriendsPath)
+      fs.readFile(renrenallfriendsPath,'utf-8',(err,data)=>{
+        if(err){
+            console.log('读取文件失败');
+            return console.log(err);
+        }else{
+            console.log(data.toString());
+            exec(data.toString())
+        }
+      })
     } else {
-      await photo.getAlbum(program.userid)
+      await photo.getAlbum(program.userid, '') 
     }
+      // var userIds = new Array("229012342","234494951", "225279695","224720335", "229415744","267089521", "240603554","347537590", "254530394","225186486"
+      //   , "243218312","230223380", "229248006","222277140");
+      // var usernames = new Array("冯燕","高冬兰", "戈范花","黄静", "金静娟","金颖", "林婷","吕霞琴", "沈喜凤","史珍珠", "孙丽花","翁芳", "吴燕","邹学敏");
+      // for (x in userIds) {
+        //await photo.getAlbum(userIds[x], usernames[x])
+      // }
   } else if (program.keyword) {
     await keyword.getPhoto(program.keyword, program.number || 10)
-  }
+  } else if (program.profile) {
+    await photo.getMyProfile()
+  } else if (program.friend) {
+    await photo.getMyFriends()
+  } else if (program.follow) {
+    await photo.getMyFollow()
+  } 
+}
+
+async function exec(jstr) {
+   const users = JSON.parse(jstr)
+   for (let i = 0; i < users.length; i++) {
+      if(i < 2) {
+        await photo.getAlbum(users[i].uid, users[i].uname)
+      }
+   }
 }
 
 run()
--- photo.js
+++ photo.js
@@ -4,7 +4,7 @@ const Request = require('axer').request
 const file = require('axer').file
 const config = require('./config')
 
-async function getPhoto(album) {
+async function getPhoto(album, dirname) {
   const cookieJarPath = path.resolve(config.storageDir, 'cookieJar.json')
   const request = new Request(cookieJarPath)
 
@@ -19,11 +19,12 @@ async function getPhoto(album) {
   for (let i = 0; i < photoListInfo.photoList.photoList.length; i++) {
     const photo = photoListInfo.photoList.photoList[i]
     // 创建相册目录
-    const albumPath = path.resolve(config.storageDir, `${album.ownerId}`, album.albumName)
+    const albumPath = path.resolve(config.storageDir, dirname, album.albumName)
     await file.mkdir(albumPath)
 
     const hdPhotoUrl = photo.url
-    const originalPhotoUrl = hdPhotoUrl.replace('large', 'original')
+    // const originalPhotoUrl = hdPhotoUrl.replace('large', 'original')
+    const originalPhotoUrl = hdPhotoUrl
 
     const photoSavePath = path.resolve(albumPath, `${photo.photoId}.jpg`)
     // 下载照片
@@ -31,7 +32,7 @@ async function getPhoto(album) {
       await request.download(originalPhotoUrl, photoSavePath)
       const checkContent = await file.cat(photoSavePath)
       if (checkContent.indexOf('DOCTYPE') > 0) {
-        console.log('不存在原图，开始下载高清图')
+        console.log('不存在原图，开始下载高清图' + originalPhotoUrl)
         await request.download(hdPhotoUrl, photoSavePath)
       }
     } catch (error) {
@@ -39,14 +40,14 @@ async function getPhoto(album) {
       await request.download(originalPhotoUrl, photoSavePath)
       const checkContent = await file.cat(photoSavePath)
       if (checkContent.indexOf('DOCTYPE') > 0) {
-        console.log('不存在原图，开始下载高清图')
+        console.log('不存在原图，开始下载高清图' + hdPhotoUrl)
         await request.download(hdPhotoUrl, photoSavePath)
       }
     } 
   }
 }
 
-async function getAlbum(ownerId) {
+async function getAlbum(ownerId, dirname) {
   const cookieJarPath = path.resolve(config.storageDir, 'cookieJar.json')
   const request = new Request(cookieJarPath)
 
@@ -58,11 +59,12 @@ async function getAlbum(ownerId) {
   const albumlistInfo = JSON.parse(albumlistContent.replace(/'/g, '"'))
   console.log(JSON.stringify(albumlistInfo, null, 2))
 
+  var dir = ownerId + dirname
   for(let i = 0; i < albumlistInfo.albumList.albumList.length; i++) {
     const album = albumlistInfo.albumList.albumList[i]
-    // 获取相册内到照片
+    // 获取相册内到照片 `${album.ownerId}`
     try {
-      await getPhoto(album) 
+      await getPhoto(album, dir) 
     } catch (error) {
       console.log('获取照片失败', error)
     }
@@ -84,8 +86,45 @@ async function getSelfAlbum() {
   delete nx
   console.log(user)
 
-  await getAlbum(user.id)
+  await getAlbum(user.id, user.id)
+}
+
+async function getMyProfile() {
+  const cookieJarPath = path.resolve(config.storageDir, 'cookieJar.json')
+  const request = new Request(cookieJarPath)
+  const homeResponse = await request.get('http://www.renren.com')
+
+  const $ = cheerio.load(homeResponse.body)
+  // 运行脚本获取数据并删除污染变量
+  let nxcontent = $('script').first().text()
+  // console.log(homeResponse.body)
+  eval(nxcontent)
+  const user = nx.user
+  delete nx
+  console.log(user)
+}
+
+async function getMyFriends() {
+// http://friend.renren.com/managefriends
+  const cookieJarPath = path.resolve(config.storageDir, 'cookieJar.json')
+  const request = new Request(cookieJarPath)
+  const friendsResponse = await request.get('http://friend.renren.com/GetFriendList.do?curpage=1&id=238551060')
+  console.log(friendsResponse.body)
+  // const $ = cheerio.load(friendsResponse.body)
+  // let nxcontent = $('script').last().text()
+  // console.log(nxcontent)
+  // eval(nxcontent)
+  // const user = nx.user
+  // delete nx
+  // console.log(user)
+}
+
+async function getMyFollow() {
+
 }
 
 exports.getSelfAlbum = getSelfAlbum
 exports.getAlbum = getAlbum
+exports.getMyProfile = getMyProfile
+exports.getMyFriends = getMyFriends
+exports.getMyFollow = getMyFollow
